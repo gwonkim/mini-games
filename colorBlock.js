deff541433d2cc984963a9f740420a3e7ab1dc01
kim, jiwon <wo41896804@gmail.com>
2022-03-26 00:13:37 +0900

    color-game(2)
     컬러 블록 생성 완료

--- colorBlock.js
+++ colorBlock.js
@@ -1,27 +1,20 @@
-const createBlock = () => {
-  const color = ["red", "blue", "yellow", "green", "purple"]; // 색상
-  // let blockColor = [];
-  // let block = document.createElement('div'); // 세로
-  let blockRow = document.createElementNS('div'); // 가로
+const color = ["red", "blue", "yellow", "green", "purple"]; // 블록 색상
 
-  for (let i = 0; i < 10; i++) {
-    // let blockColor2 = [];
-    for (let j = 0; j < 10; j++) {
-      let block1 = document.createElement("div"); // 가로
+window.onload = function() {
+  let board = document.querySelector('.board');
+
+ for (let i = 0; i < 10; i++) {
+   let blockRow = document.createElement('div'); // 블록 한 줄
+   blockRow.className = 'blockRow';
+   for (let j = 0; j < 10; j++) {
+      let block = document.createElement("div"); // 블록
+      block.className = 'block';
       let num = parseInt(Math.random() * 5); // 색상 랜덤 지정
-      
-      // blockColor2[j] = [num];
-      block1.style.backgroundColor(color[num]);
-      blockRow.appendChild(block1);
-      // console.log(blockColor2);
+      block.innerText = color[num];
+      block.style.backgroundColor = color[num];
+      blockRow.appendChild(block);
     }
-    blockRow.appendChild('<br />');
-    // blockColor[i] = blockColor2;
+    board.appendChild(blockRow);
   }
-  document.body.appendChild(blockRow);
-
-  console.log(blockColor);
 };
 
-// console.log(createBlock());
-createBlock();
