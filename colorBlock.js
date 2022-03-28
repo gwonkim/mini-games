697e144e7b74123de6d461010ca1142965ff040d
kim, jiwon <wo41896804@gmail.com>
2022-03-28 18:12:27 +0900

    color-game(5)
    버튼 오류 해결
    게임 기능 구현 완료
    추가로 게임 설명 버튼 구현중(!)
    일부 기능 및 디자인 수정 계획 중(!)

--- colorBlock.js
+++ colorBlock.js
@@ -1,29 +1,27 @@
-// const block = document.querySelector(".block");
-// const nonblock = document.querySelector(".nonblock");
-
 const color = ["red", "blue", "yellow", "green", "purple"]; // 블록 색상
-const size = 3; // 크기
+let size; // 크기
 let blockColor = []; // 블록 색상
 let checkedBlock = []; // 선택한 블록
 let answerColor; // 정답 색상
 
-const start = () => {
-  createBlock(); // 블럭 생성
-  showBlock();
-  setTimeout(question, 1000);
+const start = (num, time) => {
+  size = num;
+  createBlock(); // 블록 생성
+  showBlock(time); // 블록 보이기
+  setTimeout(question, 500+time); // 문제 생성
 };
 
 const createBlock = () => {
-  console.log('creatBlock... -----------------------------------')
   const game = document.querySelector(".game");
-  const blockBoard = document.createElement("div"); 
-  blockBoard.className = 'blockBoard'
+  const blockBoard = document.createElement("div");
+  blockBoard.className = 'blockBoard';
 
-  hiddenButton('start'); // 시작버튼 숨기기
+  hiddenButton('start3'); // 시작버튼 숨기기
+  hiddenButton('start5'); // 시작버튼 숨기기
+  hiddenButton('start10'); // 시작버튼 숨기기
   for (let r = 0; r < size; r++) {
     blockColor[r] = [];
     checkedBlock[r] = [];
-
     let rowBlock = document.createElement("div"); // RowBlock
     rowBlock.className = "rowBlock";
     
@@ -34,16 +32,15 @@ const createBlock = () => {
       newBlock.id = `block${r}${c}`;
       newBlock.addEventListener('click', onClickBlock);
       newBlock.style.background = color[num]; 
-
       blockColor[r][c]= color[num]; // 색상 저장
-      newBlock.innerText = color[num]; // 확인용
+      checkedBlock[r][c] = false; // check 초기화
 
       rowBlock.appendChild(newBlock);
     }
     blockBoard.appendChild(rowBlock);
   }
   game.appendChild(blockBoard);
-  
+
   let submitbutton = document.createElement('button');
   let restartbutton = document.createElement('button');
   
@@ -57,25 +54,8 @@ const createBlock = () => {
   game.appendChild(restartbutton);
 };
 
-// 문제
-const question = () => {
-  const text = document.querySelector('.text');
-  answerColor = color[parseInt(Math.random() * 5)]; // 정답 색깔
-  text.innerText = `${answerColor}를 선택해주세요.`;
-}
-
-/* 
-  const question = () => {
-  const game = document.querySelector(".game");
-  const text = document.createElement('p');
-  text.className = 'text';
-  answerColor = color[parseInt(Math.random() * 5)]; // 정답 색깔
-  text.innerText = `${answerColor}를 선택해주세요.`;
-  game.appendChild(text);
-} */
-
-// 블럭 잠깐 보이기
-let showBlock = () => {
+// 블록 보이기
+let showBlock = (time) => {
   for(let i=0; i<size; i++) {
     for(let j=0; j<size; j++) {
       let block = document.querySelector(`#block${i}${j}`);
@@ -85,17 +65,27 @@ let showBlock = () => {
             block.classList.add('nonblock');
             block.style.background = '#fff'
             }
-          }, 500);
+          }, time);
       }
     clearTimeout();
   }
 };
 
-// 블럭 클릭
+// 문제
+const question = () => {
+  const text = document.querySelector('.text');
+  answerColor = color[parseInt(Math.random() * 5)]; // 정답 색깔
+  text.innerText = `${answerColor}를 선택해주세요.`;
+};
+
+// 블록 클릭
 const onClickBlock = (e) => {
-  let classN = e.target.classList[0];
   let id = e.target.id;
+  checkAnswer(id);
+  let classN = e.target.classList[0];
   let block = document.querySelector(`#${id}`);
+
+  // class change
   if (classN === 'nonblock') {
     block.classList.remove('nonblock');
     block.classList.add('block');
@@ -105,41 +95,35 @@ const onClickBlock = (e) => {
     block.classList.add('nonblock');
     block.style.background = '#fff';
   }
+};
 
-  // 정답 확인
-  checkAnswer(id);
-}
-
+// 정답 확인
 const checkAnswer = (id) => {
   if (blockColor[id[id.length-2]][id[id.length-1]] !== answerColor) {
-    console.log('틀렸습니다.');
     alert('틀렸습니다!');
     onRestart();
-  } else {
-    console.log('answerColor : ', blockColor[id[id.length-2]][id[id.length-1]], '을 잘 선택하였습니다.')
+  } else { // 정답 체크
+    checkedBlock[id[id.length-2]][id[id.length-1]] = true;
   }
-}
+};
 
 // 재시작
 const onRestart = () => {
-  document.querySelector('.blockBoard').remove();
-  showButton('start');
-  removeButton('text');
-  removeButton('submit');
-  removeButton('restart');
+  location.reload();
 }
 
 // 제출하기
-const submit = (e) => {
-  console.log('submit', submit);
-  console.log("e", e);
-  console.log("e", e.target);
-  let answer = blockColor.filter(v => (v === answerColor ? true : false));
-  console.log('정답!', answer);
-
-  console.log("blockColor", blockColor);
-  console.log("answerColor", answerColor);
-  // answerColor;
+const submit = () => {
+  for (let r = 0; r < size; r++) {
+    for (let c = 0; c < size; c++) {
+      if(blockColor[r][c] === answerColor && !checkedBlock[r][c]){
+        alert('정답이 아닙니다.');
+        return onRestart();
+      }
+    }
+  }
+  alert('정답입니다.');
+  return onRestart();
 };
 
 // 버튼 숨기기
@@ -156,5 +140,4 @@ const removeButton = (value) => {
 const showButton = (value) => {
   let tag = document.querySelector(`.${value}`);
   tag.style.visibility = 'visible';
-  // tag.style.display = 'block';
 };
